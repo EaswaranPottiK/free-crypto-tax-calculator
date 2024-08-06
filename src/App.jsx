9867630c8c9e22d1b10993dadecc555f2612b0d9
Easwaran <easwaranpottiak@gmail.com>
2024-08-06 22:47:38 +0530

    UI changes: mobile view friendly

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='bg-[#edeeef] min-h-[100vh] h-[100%]'>
       <div className='h-8'></div>
-      <div className='bg-white w-[60%] m-auto pt-3 rounded-lg border'>
+      <div className='bg-white md:w-[60%] m-auto pt-3 rounded-lg border'>
         <CryptoPage/>
       </div>
     </div>
--- src/CryptoPage.jsx
+++ src/CryptoPage.jsx
@@ -3,45 +3,64 @@ import React, { useReducer, useState } from 'react'
 const CryptoPage = () => {
   // 0 for less than 12 month and 1 for more than 12 month
   const [investmentType, setInvestmentType] = useState(1)
-  // const tickmark = `?&#10003;`
+  
   function reducer(state, action){
     let itemToReturn = {...state,...action}
+
+    if(investmentType){
+      if((Number(itemToReturn.salePrice) - Number(itemToReturn.purchasePrice)- Number(itemToReturn.expenses))>0){
+        itemToReturn.greenbox = (Number(itemToReturn.salePrice) - Number(itemToReturn.purchasePrice)- Number(itemToReturn.expenses))*0.5
+      }
+      else {
+        itemToReturn.greenbox = Number(itemToReturn.salePrice) - Number(itemToReturn.purchasePrice)- Number(itemToReturn.expenses)
+      }
+    }
+    else {
+      itemToReturn.greenbox = Number(itemToReturn.salePrice) - Number(itemToReturn.purchasePrice)- Number(itemToReturn.expenses)
+    }
+    
+    
     if (itemToReturn.annualIncome <=18200){
       itemToReturn.taxRate = '0%'
+      itemToReturn.bluebox = 0
     }
     else if(itemToReturn.annualIncome <=45200){
       itemToReturn.taxRate = 'Nill + 19% of excess over $18,200'
+      itemToReturn.bluebox = Number(itemToReturn.greenbox)*0.19
     }
     else if(itemToReturn.annualIncome <=120000){
       itemToReturn.taxRate = '$5,092 + 32.5% of excess over $45,000'
+      itemToReturn.bluebox = Number(itemToReturn.greenbox)*0.325 + 5092
     }
     else if(itemToReturn.annualIncome <=180000){
       itemToReturn.taxRate = '$29,467 + 37% of excess over $120,000'
+      itemToReturn.bluebox = Number(itemToReturn.greenbox)*0.37 + 29467
     }
     else{
       itemToReturn.taxRate = '$51,667 + 45% of excess over $180,000'
+      itemToReturn.bluebox = Number(itemToReturn.greenbox)*0.45 + 51667
     }
     return itemToReturn
   }
-  const [state,dispatch] = useReducer(reducer, {taxRate:'0%',purchasePrice:0,salePrice:0,expenses:0,annualIncome:18200,capitalGainAmount:0,discountForLongTermGains:0})
 
-  console.log(state)
+  const [state,dispatch] = useReducer(reducer, {greenbox:0,bluebox:0, taxRate:'0%',purchasePrice:0,salePrice:0,expenses:0,annualIncome:18200,capitalGainAmount:0,discountForLongTermGains:0})
 
   return (
     <div className='mt-8 '>
       <p className='text-center text-2xl font-black'>Free Crypto Tax Calculator Australia</p>
 
-      <div className='flex justify-around mt-8'>
-        <div>
+      {/* Financial year and country */}
+      <div className='flex justify-between md:justify-around mt-8 px-2 md:px-0'>
+        <div className='flex flex-col md:ml-3  min-w-40'>
           Financial Year 
-          <select className='min-w-52 min-h-12 ml-3  bg-[#edeeef] rounded-lg'>
+          <select className='md:min-w-52 min-h-12 bg-[#edeeef] rounded-lg'>
             <option value="fixed">FY 2023-34</option>
           </select> 
         </div>
-        <div>
+        <div className='flex flex-col md:ml-3 min-w-40'>
           Country 
           {/* <Flag name='myanmar' /> */}
-          <select className='min-w-52 min-h-12 ml-3  bg-[#edeeef] rounded-lg'>
+          <select className='md:min-w-52 min-h-12 bg-[#edeeef] rounded-lg'>
           <option value="fixed">Australia</option>
           </select> 
         </div>
@@ -52,41 +71,43 @@ const CryptoPage = () => {
       <div>
         {/* one line of question ie 2 questions */}
         <br/>
-        <div className='flex justify-around'>
+        <div className='flex justify-around md:flex-row flex-col '>
 
           <div className='flex flex-col text-sm'>
             <>Enter purchase price of Crypto</>
-            <div className='flex w-52 text-lg'>
+            <div className='flex md:w-52 text-lg w-[100%]'>
               <input type='text' value=' $' className='w-4 min-h-12  bg-[#edeeef] rounded-l-lg' readOnly/>
-              <input type='number' onChange={(e)=>dispatch({purchasePrice:e.target.value})}  className='w-48 min-h-12 outline-none bg-[#edeeef] rounded-r-lg'/>
+              <input type='number' onChange={(e)=>dispatch({purchasePrice:e.target.value})}  className='w-[100%] md:w-48 min-h-12 outline-none bg-[#edeeef] rounded-r-lg'/>
               {/* onChange={(e)=>{}} */}
             </div>
           </div>
 
           <div className='flex flex-col text-sm'>
             <>Enter sale price of Crypto</>
-            <div className='flex  w-52 text-lg'>
+            <div className='flex  md:w-52 text-lg w-[100%]'>
               <input type='text' value=' $' className='w-4 min-h-12  bg-[#edeeef] rounded-l-lg' readOnly/>
-              <input type='number'  onChange={(e)=>dispatch({salePrice:e.target.value})}  className='w-48 min-h-12 outline-none bg-[#edeeef] rounded-r-lg'/>
+              <input type='number'  onChange={(e)=>dispatch({salePrice:e.target.value})}  className='w-[100%] md:w-48 min-h-12 outline-none bg-[#edeeef] rounded-r-lg'/>
             </div>
           </div>
 
         </div>
 
         <br/>
-        <div className='flex justify-around'>
+        <div className='flex justify-around flex-col md:flex-row'>
           <div className='flex flex-col text-sm'>
             <>Enter your Expenses</>
-            <div className='flex  w-52 text-lg'>
+            <div className='flex w-[100%] md:w-52 text-lg'>
               <input type='text' value=' $' className='w-4 min-h-12  bg-[#edeeef] rounded-l-lg' readOnly/>
-              <input type='number'  onChange={(e)=>dispatch({expenses:e.target.value})}  className='w-48 min-h-12 outline-none bg-[#edeeef] rounded-r-lg'/>
+              <input type='number'  onChange={(e)=>dispatch({expenses:e.target.value})}  className='w-[100%] md:w-48 min-h-12 outline-none bg-[#edeeef] rounded-r-lg'/>
             </div>
           </div>
 
+          <br className='md:hidden'></br>
+          
           <div className='flex flex-col text-sm'>
             <>Investment Type</>
             <div>
-              <div className='flex gap-1'>
+              <div className='flex justify-around'>
                 <button onClick={()=>{setInvestmentType(0)}} className={`rounded-xl pr-7 pl-3 py-3 border ${investmentType ==0 ?"border-blue-600 text-blue-600":""}`}>Short Term</button>
                 <button onClick={()=>{setInvestmentType(1)}} className={`rounded-xl pr-7 pl-3 py-3 border ${investmentType >0 ?"border-blue-600 text-blue-600":""}`}>Long Term</button>
               </div>
@@ -101,11 +122,11 @@ const CryptoPage = () => {
         </div>
 
         <br />
-        <div className='flex justify-around'>
+        <div className='flex justify-around flex-col md:flex-row'>
           <div className='flex flex-col text-sm'>
             <>Select your annual income </>
             <div>
-              <select  onChange={(e)=>dispatch({annualIncome:e.target.value})}  className='flex  w-52 text-lg bg-[#edeeef] h-12 rounded-md'>
+              <select  onChange={(e)=>dispatch({annualIncome:e.target.value})}  className='flex w-[100%] md:w-52 text-lg bg-[#edeeef] h-12 rounded-md'>
                 <option value={18200}>$0 - $18,200</option>
                 <option value={45000}>$18,201 - $45,000</option>
                 <option value={120000}>$45,001 - $120,000</option>
@@ -115,8 +136,8 @@ const CryptoPage = () => {
             </div>
           </div>
 
-          <div className='flex flex-col text-sm w-52'>
-            <p>Tax Rate</p>
+          <div className='flex flex-row md:flex-col text-sm w-52'>
+            <p>Tax Rate &nbsp;</p>
             <p>{state.taxRate}</p>
           </div>
         </div>
@@ -149,11 +170,11 @@ const CryptoPage = () => {
           <div className='flex justify-around'>
             <div className='mt-10 rounded-md px-8 py-2 bg-[#EBF9F4] w-1/4'>
               <p className='text-center '>Net capital gains tax amount</p>
-              <p className='text-center text-[#1DBE8A] font-extrabold text-2xl '> $ </p>
+              <p className='text-center text-[#1DBE8A] font-extrabold text-2xl '> $ {state.greenbox} </p>
             </div>
             <div className='mt-10 rounded-md px-8 py-2 bg-[#EBF2FF] w-1/4'>
               <p className='text-center '>Tax you need to pay*</p>
-              <p className='text-center text-[#0141CF] font-extrabold text-2xl '> $ 123</p>
+              <p className='text-center text-[#0141CF] font-extrabold text-2xl '> $ {state.bluebox}</p>
             </div>
           </div>    
 
